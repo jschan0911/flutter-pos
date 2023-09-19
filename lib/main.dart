590936b69a1a0c67a739e2e2a6bfd09f509ab891
jeongseungchan <jschan0911@naver.com>
2023-09-19 20:28:19 +0900

    refactor: 프로젝트 명 변경

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "Eunhyun-KIOSK",
+      title: "Eunhyun-POS",
       home: MainPage(),
       // routes: routes,
     );
